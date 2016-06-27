cd738250cde56cbeacfa026564694148ef938288
qwe757964954 <757964954@qq.com>
2016-06-27 15:19:23 +0800

    iOS直播demo

---
+++ JX-MediaDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JX-MediaDemo
+//
+//  Created by 建星 on 16/6/27.
+//  Copyright © 2016年 建星. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JX-MediaDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JX-MediaDemo
+//
+//  Created by 建星 on 16/6/27.
+//  Copyright © 2016年 建星. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
